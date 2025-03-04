9c793bd0c13d6ef20c8cd005606eb8fadad7ea9a
10922162 <a0978615949z@gmail.com>
2025-03-04 20:11:56 +0800

    2

---
+++ 20250304/hw4-2.py
@@ -0,0 +1,42 @@
+def count_keyword(lyrics, word='bridge'):
+    return lyrics.lower().split().count(word)  # 轉小寫並計算單字出現次數
+
+# 歌詞
+lyrics = '''
+When you're weary
+Feeling small
+When tears are in your eyes
+I will dry them all
+I'm on your side
+Oh, when times get rough
+And friends just can't be found
+Like a bridge over troubled water
+I will lay me down
+Like a bridge over troubled water
+I will lay me down
+When you're down and out
+When you're on the street
+When evening falls so hard
+I will comfort you
+I'll take your part
+Oh, when darkness comes
+And pain is all around
+Like a bridge over troubled water
+I will lay me down
+Like a bridge over troubled water
+I will lay me down
+Sail on, silver girl
+Sail on by
+Your time has come to shine
+All your dreams are on their way
+See how they shine
+Oh, if you need a friend
+I'm sailing right behind
+Like a bridge over troubled water
+I will ease your mind
+Like a bridge over troubled water
+I will ease your mind
+'''
+
+# 計算 'bridge' 出現的次數
+print("The word 'bridge' appears:", count_keyword(lyrics, 'bridge'), "times.")
